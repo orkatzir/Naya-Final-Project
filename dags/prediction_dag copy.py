33168452b1d74d0b4843485a00f8094a17baf1b3
orkatzir <orkatzir@gmail.com>
2023-07-05 18:58:38 +0000

    Fixed Bugs

---
+++ dags/prediction_dag copy.py
@@ -0,0 +1,21 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.ssh.operators.ssh import SSHOperator
+default_args = {
+    'owner': 'ork',
+    'depends_on_past': False,
+    'retries': 0
+}
+
+with DAG(
+    dag_id='prediction_dag',
+    default_args=default_args,
+    description='Stock Prediction',
+    start_date=datetime(2020, 7, 29, 2),
+    schedule_interval='30 2 * * *'
+) as dag:
+    task_1=SSHOperator(
+        task_id="ssh_run_prediction",
+        ssh_conn_id='ssh_default',
+        command='python /home/naya/Final_Project/read_mongo.py'
+    )
